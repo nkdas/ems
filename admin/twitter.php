2c415de65e819b6247f6abded9646edf9758e143
Neeraj Kumar Das <nkdas91@gmail.com>
2016-02-17 13:12:00 +0530

    added functionality for the admin to view users tweets

--- admin/twitter.php
+++ admin/twitter.php
@@ -1,16 +1,20 @@
 <?php
 require_once((dirname(__DIR__)) . '/resources/db_connection.php');
-
-$query = mysqli_query($connection, "SELECT firstName, twitterId
-    FROM employee_details");
-$record = array();
-if ($query) {
-    while ($row = mysqli_fetch_assoc($query)) {
-        array_push($record, $row['firstName'] . ' ' . $row['twitterId']);
-    }
+if (!isset($_SESSION['pk_admin'])) {
+    header("Location: index.php");
 }
+else {
+    $query = mysqli_query($connection, "SELECT firstName, twitterId
+        FROM employee_details");
+    $record = array();
+    if ($query) {
+        while ($row = mysqli_fetch_assoc($query)) {
+            array_push($record, $row['firstName'] . ' ' . $row['twitterId']);
+        }
+    }
 
-require((dirname(__DIR__)) . '/layout/header.php');
+    require((dirname(__DIR__)) . '/layout/header.php');
+}
 ?>
 <body onload="viewTwitter();">
     <nav class="navbar navbar-inverse" data-spy="affix">
@@ -37,21 +41,21 @@ require((dirname(__DIR__)) . '/layout/header.php');
         </div>
     </nav>
     <div id="sectionAdmin" class="container-fluid">
-    	<div class="row">
-    		<div class="col-md-12">
-    			<h3>Select a user to view his/her tweets</h3>
-    		</div>
-    	</div>
-    	<div class="row">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Select a user to view his/her tweets</h3>
+            </div>
+        </div>
+        <div class="row">
             <div class="col-md-3">
-            	<?php
-            	echo '<select id="userList" name="userList" onchange="viewTwitter()" class="form-control">';
-			    foreach ($record as $key => $value) {
-			    	$data = explode(" ", $value);
-			        echo '<option value="' . $data[1] . '">' . $data[0] . '</option>';
-			    }
-			    echo '</select>';
-            	?>
+                <?php
+                echo '<select id="userList" name="userList" onchange="viewTwitter()" class="form-control">';
+                foreach ($record as $key => $value) {
+                    $data = explode(" ", $value);
+                    echo '<option value="' . $data[1] . '">' . $data[0] . '</option>';
+                }
+                echo '</select>';
+                ?>
             </div>
             <div id="twitterDiv" class="col-md-4">
 
